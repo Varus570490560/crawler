20d3460fe5752de771e85635d00a4e1cd163d477
XuShuo <xushuo@highlight.mobi>
2022-01-25 11:16:23 +0800

    所有的模块url都进行了抽取

--- src/crawling_sub_comment.py
+++ src/crawling_sub_comment.py
@@ -6,8 +6,7 @@ import analysis
 import connect_mysql
 
 
-def crawling_sub_comment_by_id(comment_id, is_print):
-    url = 'https://www.taptap.io/webapiv2/review-comment/v1/by-review'
+def crawling_sub_comment_by_id(comment_id, is_print, sub_comment_url):
     param_from = 0
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 '
@@ -22,7 +21,7 @@ def crawling_sub_comment_by_id(comment_id, is_print):
             'from': str(param_from),
             'X-UA': 'V=1&PN=WebAppIntl&LANG=zh_TW&VN_CODE=59&VN=0.1.0&LOC=CN&PLT=PC&DS=Android&UID=22a3964e-db21-41a2-852d-f30283cda0f3&VID=434092598&DT=PC'
         }
-        response = requests.get(url=url, headers=headers, params=params)
+        response = requests.get(url=sub_comment_url, headers=headers, params=params)
         response_json = response.json()
         if len(response_json['data']['list']) != 0:
             response_jsons.append(response_json)
@@ -40,9 +39,9 @@ def crawling_sub_comment_by_id(comment_id, is_print):
             param_from += 10
 
 
-def crawling_sub_comment_by_ids_and_app_ids(ids_and_app_ids, is_print, db):
+def crawling_sub_comment_by_ids_and_app_ids(ids_and_app_ids, is_print, db, sub_comment_url):
     for id_and_app_id in ids_and_app_ids:
-        response_jsons = crawling_sub_comment_by_id(id_and_app_id[0], is_print)
+        response_jsons = crawling_sub_comment_by_id(id_and_app_id[0], is_print, sub_comment_url=sub_comment_url)
         for response_json in response_jsons:
             analysis_tuples = analysis.analysis_game_sub_comment_json_to_tuple(response_json, id_and_app_id)
             for analysis_tuple in analysis_tuples:
